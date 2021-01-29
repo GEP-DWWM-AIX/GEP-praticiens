1e09b0989f18ae2741c55b2307ab56b6a57c17ee
simplon-marseille <acardnicolas@hotmail.com>
2021-01-29 10:42:31 +0100

    url update

--- app.js
+++ app.js
@@ -5,43 +5,7 @@ const spe = document.querySelector('#specialite');
 const button = document.querySelector('button');
 const visiteCheck = document.querySelector('#visite');
 
-
-const getData = () => {
-    const urlPraticien = 'http://localhost:90/gsb/praticien';
-    let etatVisite;
-    let ville;
-    fetch(urlPraticien)
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data);
-            data.forEach(prat => {
-                if(prat.visite){
-                    etatVisite = 'Pas encore visité';
-                }else{
-                    etatVisite = 'Déja visité';
-                }
-                let villeArray = prat.adresse.split(' ');
-                ville = villeArray[villeArray.length - 1];
-                    listPraticien.insertAdjacentHTML('beforeend', `
-                    <div class="col-xs-12 col-md-6 center">
-                        <div class="card-praticien" id="${prat.id}">
-                            <div class="avatar">
-                                <img src="./assets/images/dentiste.jpg" class="avatar" alt="">
-                                <div class="nom">${prat.nom} ${prat.prenom}</div>
-                            </div>            
-                            <div class="specialite">${prat.specialite}</div>
-                            <div class="visite">${etatVisite}</div>
-                            <div class="localite">${ville}</div>
-                        </div>
-                    </div>
-                `);
-            });
-        });
-};
-
-
 const getBySpeLoc = (myUrl) => {
-    //const urlPraticien = 'http://localhost:90/gsb/praticien';
     console.log(myUrl);
     let countPrat = 0;
     let etatVisite;
@@ -60,7 +24,7 @@ const getBySpeLoc = (myUrl) => {
                 ville = villeArray[villeArray.length - 1];
                     countPrat++;
                     listPraticien.insertAdjacentHTML('beforeend', `
-                    <div class="col-xs-12 col-md-6 center">
+                    <div class="col-xs-12 col-md-6 myCard center">
                         <div class="card-praticien" id="${prat.id}">
                             <div class="avatar">
                                 <img src="./assets/images/dentiste.jpg" class="avatar" alt="">
@@ -77,6 +41,12 @@ const getBySpeLoc = (myUrl) => {
                 const card = document.querySelector('.card-praticien');
                 card.parentNode.classList.remove('col-md-6');
             }
+            const cards = document.querySelectorAll('.myCard');
+            cards.forEach((card) => {
+                card.addEventListener('click', (event) => {
+                    document.querySelector('.praticiens').style.display = 'none';
+                });
+            });
         });
 };
 
@@ -106,7 +76,5 @@ button.addEventListener('click', (event) => {
     spe.value = '';
 
 });
-
-//getData();
 
 
